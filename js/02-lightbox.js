44a6c0ba6bb0282e7aeefc0202d32aba55ec555a
AdrianG24 <adriangolodisin@gmail.com>
2022-09-25 13:32:24 +0300

    finished tasks

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+createGallery();
+
+gallery.addEventListener("click", (event) => {
+  event.preventDefault();
+});
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGallery() {
+  let markup = galleryItems
+    .map(
+      ({ preview, original, description }) => `
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+    )
+    .join("\n");
+
+  gallery.insertAdjacentHTML("beforeend", markup);
+}
